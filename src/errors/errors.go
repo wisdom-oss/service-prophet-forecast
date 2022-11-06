b83e73349131310d3da38e7450af298d7d9078d8
Jan Eike Suchard <jan-eike.suchard@magenta.de>
2022-11-06 18:58:17 +0100

    chore: added new errors for to the service

--- src/errors/errors.go
+++ src/errors/errors.go
@@ -17,6 +17,9 @@ const UnsupportedHTTPMethod = "UNSUPPORTED_METHOD"
 const DatabaseQueryError = "DATABASE_QUERY_ERROR"
 const UnprocessableEntity = "UNPROCESSABLE_ENTITY"
 const UniqueConstraintViolation = "UNIQUE_CONSTRAINT_VIOLATION"
+const MissingRequiredParameter = "MISSING_REQUEST_PARAMETER"
+const DataWriting = "INTERNAL_DATA_WRITE_ERROR"
+const RScriptError = "R_SCRIPT_EXECUTION_ERROR"
 
 var errorTitle = map[string]string{
 	UnauthorizedRequest:       "Unauthorized Request",
@@ -25,11 +28,14 @@ var errorTitle = map[string]string{
 	DatabaseQueryError:        "Database Query Error",
 	UnprocessableEntity:       "Unprocessable Entity",
 	UniqueConstraintViolation: "Unique Constraint Violation",
+	MissingRequiredParameter:  "Missing required request parameter",
+	DataWriting:               "Internal data writing error",
+	RScriptError:              "R Script execution error",
 }
 
 var errorDescription = map[string]string{
 	UnauthorizedRequest: "The resource you tried to access requires authorization. Please check your request",
-	MissingScope: "Yu tried to access a resource which is protected by a scope. " +
+	MissingScope: "You tried to access a resource which is protected by a scope. " +
 		"Your authorization information did not contain the required scope.",
 	UnsupportedHTTPMethod: "The used HTTP method is not supported by this microservice. " +
 		"Please check the documentation for further information",
@@ -38,6 +44,10 @@ var errorDescription = map[string]string{
 	UnprocessableEntity: "The JSON object you sent to the service is not processable. Please check your request",
 	UniqueConstraintViolation: "The object you are trying to create already exists in the database. " +
 		"Please check your request and the documentation",
+	MissingRequiredParameter: "The request you sent was not processable, " +
+		"due to a missing parameter in the request. Please check the documentation",
+	DataWriting:  "A file needed for making the prognosis could not be written",
+	RScriptError: "An error occurred while trying to execute the underlying R script. Please check the server logs",
 }
 
 var httpStatus = map[string]int{
@@ -47,6 +57,9 @@ var httpStatus = map[string]int{
 	DatabaseQueryError:        http.StatusInternalServerError,
 	UnprocessableEntity:       http.StatusUnprocessableEntity,
 	UniqueConstraintViolation: http.StatusConflict,
+	MissingRequiredParameter:  http.StatusBadRequest,
+	DataWriting:               http.StatusInternalServerError,
+	RScriptError:              http.StatusInternalServerError,
 }
 
 func NewRequestError(errorCode string) structs.RequestError {
