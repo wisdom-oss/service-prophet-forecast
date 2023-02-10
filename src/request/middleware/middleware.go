68bb2b92862e86b56fb6161e3416d1d51ef27034
Jan Eike Suchard <jan-eike.suchard@magenta.de>
2023-02-10 16:42:10 +0100

    chore: updating middleware documentation and adding parser for query parameters

--- src/request/middleware/middleware.go
+++ src/request/middleware/middleware.go
@@ -1,6 +1,7 @@
 package middleware
 
 import (
+	"context"
 	"github.com/go-chi/chi/v5/middleware"
 	log "github.com/sirupsen/logrus"
 	e "microservice/request/error"
@@ -10,6 +11,7 @@ import (
 	"strings"
 )
 
+// AuthorizationCheck checks the incoming Authorization header from the API-Gateway
 func AuthorizationCheck(nextHandler http.Handler) http.Handler {
 	return http.HandlerFunc(
 		func(responseWriter http.ResponseWriter, request *http.Request) {
@@ -47,12 +49,30 @@ func AuthorizationCheck(nextHandler http.Handler) http.Handler {
 	)
 }
 
+// AdditionalResponseHeaders puts the request id and other aditional headers into the response
 func AdditionalResponseHeaders(nextHandler http.Handler) http.Handler {
 	return http.HandlerFunc(
 		func(responseWriter http.ResponseWriter, request *http.Request) {
 			requestID := middleware.GetReqID(request.Context())
 			responseWriter.Header().Set("X-Request-ID", requestID)
 			nextHandler.ServeHTTP(responseWriter, request)
+		},
+	)
+}
+
+// ParseQueryParametersToContext reads all query parameters from the request URL and puts it into the context of the
+// request
+func ParseQueryParametersToContext(nextHandler http.Handler) http.Handler {
+	return http.HandlerFunc(
+		func(responseWriter http.ResponseWriter, request *http.Request) {
+			// get the request context
+			ctx := request.Context()
+			// iterate through the query parameters and add the values to the request context
+			for key, value := range request.URL.Query() {
+				ctx = context.WithValue(ctx, key, value)
+			}
+			// now serve the request to the next handler
+			nextHandler.ServeHTTP(responseWriter, request.WithContext(ctx))
 		},
 	)
 }
