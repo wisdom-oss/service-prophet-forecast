43f0246b31c23a2f9cb3d984876d64c67fb20418
Jan Eike Suchard <jan-eike.suchard@magenta.de>
2023-02-14 00:23:52 +0100

    refactor: removing unnecessary logging

--- src/request/middleware/middleware.go
+++ src/request/middleware/middleware.go
@@ -3,7 +3,6 @@ package middleware
 import (
 	"context"
 	"github.com/go-chi/chi/v5/middleware"
-	log "github.com/sirupsen/logrus"
 	e "microservice/request/error"
 	"microservice/utils"
 	"microservice/vars"
@@ -15,13 +14,6 @@ import (
 func AuthorizationCheck(nextHandler http.Handler) http.Handler {
 	return http.HandlerFunc(
 		func(responseWriter http.ResponseWriter, request *http.Request) {
-			logger := log.WithFields(
-				log.Fields{
-					"middleware": true,
-					"title":      "AuthorizationCheck",
-				},
-			)
-			logger.Debug("Checking the incoming request for authorization information set by the gateway")
 			if request.URL.Path == "/healthcheck" {
 				nextHandler.ServeHTTP(responseWriter, request)
 				return
@@ -30,7 +22,6 @@ func AuthorizationCheck(nextHandler http.Handler) http.Handler {
 			scopes := request.Header.Get("X-Authenticated-Scope")
 			// Check if the string is empty
 			if strings.TrimSpace(scopes) == "" {
-				logger.Warning("Unauthorized request detected. The required header had no content or was not set")
 				err, _ := e.BuildRequestError(e.MissingAuthorizationInformation)
 				e.RespondWithRequestError(err, responseWriter)
 				return
@@ -38,7 +29,6 @@ func AuthorizationCheck(nextHandler http.Handler) http.Handler {
 
 			scopeList := strings.Split(scopes, ",")
 			if !utils.ArrayContains(scopeList, vars.ScopeConfiguration.ScopeValue) {
-				logger.Error("Request rejected. The user is missing the scope needed for accessing this service")
 				err, _ := e.BuildRequestError(e.InsufficientScope)
 				e.RespondWithRequestError(err, responseWriter)
 				return
